92199559c70fb9516918f7f89c5d29877b8556ff
Sonya Nadtochey <nadtocheysonya@gmail.com>
2024-05-27 16:24:55 +0300

    строка поиска и рабочая кнопка заказать

--- hello-page.js
+++ hello-page.js
@@ -1,24 +1,3 @@
-// document.getElementById('filter-button').addEventListener('click', function() {
-//     // Получаем все чекбоксы
-//     const checkboxes = document.querySelectorAll('.filter input[type="checkbox"]');
-//     const selectedGenres = [];
-
-//     // Проходим по каждому чекбоксу и проверяем, отмечен ли он
-//     checkboxes.forEach((checkbox) => {
-//         if (checkbox.checked) {
-//             selectedGenres.push(checkbox.value);
-//         }
-//     });
-
-//     // Выводим выбранные жанры в консоль
-//     console.log('Selected genres:', selectedGenres);
-
-//     // Отображаем выбранные жанры на странице
-//     const selectedGenresList = document.getElementById('selected-genres');
-//     selectedGenresList.innerHTML = '';
-//     selectedGenres.forEach((genre) => {
-//         const listItem = document.createElement('div');
-//         listItem.textContent = genre;
-//         selectedGenresList.appendChild(listItem);
-//     });
-// });
+document.getElementById('like').addEventListener('click', function() {
+    window.location.href = 'vhod.html';
+})
