5c7f42acf6aea537b6cb7371a266d4cce166dac3
Sonya Nadtochey <nadtocheysonya@gmail.com>
2024-05-28 19:16:35 +0300

    модальное окно

--- hello-page.js
+++ hello-page.js
@@ -1,3 +1,9 @@
-document.getElementById('like').addEventListener('click', function() {
-    window.location.href = 'vhod.html';
-})
+// document.getElementById('like').addEventListener('click', function() {
+//     window.location.href = 'vhod.html';
+// })
+
+$(document).ready(function() {
+    $('#like').click(function() {
+        $('#myModal').modal('show');
+    });
+});
